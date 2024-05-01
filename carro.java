c0c354500aa8001189daf591a84ef0f4d3439c43
Larissa Irene Aguiar <83132717+larissa92@users.noreply.github.com>
2024-05-01 07:21:53 -0300

    Update carro.java
    construtor personalizado

--- carro.java
+++ carro.java
@@ -2,3 +2,6 @@ public class Carro {
     private String marca;
     private String modelo;
     private int ano;
+ // Construtor personalizado
+    public Carro(String marca, String modelo, int ano) {
+        this.marca = marca;
