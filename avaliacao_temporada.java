a2e1da97e34a3be05dd7c75aea22e32efec794d0
Larissa Irene Aguiar <83132717+larissa92@users.noreply.github.com>
2024-05-05 06:35:19 -0300

    Update avaliacao_temporada.java
    adicionando comentários a série

--- avaliacao_temporada.java
+++ avaliacao_temporada.java
@@ -3,3 +3,16 @@ Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
         .collect(Collectors.groupingBy(Episodio::getTemporada,
                 Collectors.averagingDouble(Episodio::getAvaliacao)));
 System.out.println(avaliacoesPorTemporada);
+
+// System.out.println("Digite um trecho do título do episódio");
+// var trechoTitulo = leitura.nextLine();
+// Optional<Episodio> episodioBuscado = episodios.stream()
+//         .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+//         .findFirst();
+// if(episodioBuscado.isPresent()){
+//     System.out.println("Episódio encontrado!");
+//     System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
+// } else {
+//     System.out.println("Episódio não encontrado!");
+// }
+
